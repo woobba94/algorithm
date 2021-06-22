7b2a859bd7ee757ea373a3e6cc24e89984b1877c
woobba94 <jwj3199@naver.com>
2021-06-22 10:04:19 +0900

    update

---
+++ Project_codingTest/baekjoon_step_1_08.cpp
@@ -0,0 +1,25 @@
+////문제
+////두 정수 A와 B를 입력받은 다음, A / B를 출력하는 프로그램을 작성하시오.
+////
+////입력
+////첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+////
+////출력
+////첫째 줄에 A / B를 출력한다.실제 정답과 출력값의 절대오차 또는 상대오차가 10 - 9 이하이면 정답이다.
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int a, b;
+//	double result;
+//	while (1)
+//	{
+//		cin >> a >> b;
+//		if (0 < a && b < 10) {
+//			result = (double)a / (double)b;
+//			break;
+//		}
+//	}
+//	cout.precision(15);
+//	cout << result;
+//}
