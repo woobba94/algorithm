beb76848fc83ebb68130f1781bedc6abd4422ce9
woobba94 <jwj3199@naver.com>
2021-06-22 23:20:10 +0900

    update

---
+++ Project_codingTest/baekjoon_step_4_03.cpp
@@ -0,0 +1,36 @@
+//#include <iostream>
+//using namespace std;
+//
+//int factorial(int n);
+//int main()
+//{
+//	int n = 0;
+//
+//	while (1)
+//	{
+//		cin >> n;
+//		if (n < 0 || 12 < n)
+//		{
+//			cout << "범위초과 다시입력 :";
+//			continue;
+//		}
+//		else
+//		{
+//			cout << factorial(n);
+//			break;
+//		}
+//	}
+//}
+//
+//int factorial(int n)
+//{
+//	int result = 0;
+//
+//	if (0 < n)
+//	{
+//		result = n * factorial(n - 1);
+//	}
+//	else return 1;
+//	
+//	return result;
+//}
