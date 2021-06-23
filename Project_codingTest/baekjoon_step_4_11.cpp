3f6748570d72df3fd97c19acd9d0a2d7bc6df605
woobba94 <66201264+woobba94@users.noreply.github.com>
2021-06-23 15:01:17 +0900

    9020 clear

--- Project_codingTest/baekjoon_step_4_11.cpp
+++ Project_codingTest/baekjoon_step_4_11.cpp
@@ -1,66 +1,66 @@
-#include <iostream>
-using namespace std;
-
-bool isPrime(int num);
-void findPartition(int num);
-int main()
-{
-	int T;
-	int arr[100];
-	
-	cout << "케이스 갯수 : ";
-	cin >> T;
-
-	cout << "2보다 큰 짝수 입력 : " << endl;
-
-	for (int i = 0; i < T; i++)
-	{
-		cin >> arr[i];
-	}
-	for (int i = 0; i < T; i++)
-	{
-		findPartition(arr[i]);
-	}
-}
-
-bool isPrime(int num) // 소수 판별
-{
-	if (num == 1) return false;
-
-	for (int i = 2; i <= sqrt(num); i++)
-	{
-		if (num % i == 0) return false;
-	}
-	return true;
-}
-
-void findPartition(int num) // 파티션 계산&출력
-{
-	int result[3] = {0,};//i값, j값, 차이값 저장
-
-	for (int i = 1; i < num; i++)
-	{
-		for (int j = num - 1; j > 0; j--)
-		{
-			if (isPrime(i) == true && isPrime(j) == true)
-			{
-				if (i + j == num) {
-					if (result[0] == 0) //첫 실행
-					{
-						result[0] = i;
-						result[1] = j;
-						result[2] = abs(i - j);
-					}
-					else if (result[2] > abs(i - j))
-					{
-						result[0] = i;
-						result[1] = j;
-						result[2] = abs(i - j);
-					}
-					else continue;
-				}
-			}
-		}
-	}
-	cout << result[0] << " " << result[1] << endl;
-}
+//#include <iostream>
+//using namespace std;
+//
+//bool isPrime(int num);
+//void findPartition(int num);
+//int main()
+//{
+//	int T;
+//	int arr[100];
+//	
+//	cout << "케이스 갯수 : ";
+//	cin >> T;
+//
+//	cout << "2보다 큰 짝수 입력 : " << endl;
+//
+//	for (int i = 0; i < T; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//	for (int i = 0; i < T; i++)
+//	{
+//		findPartition(arr[i]);
+//	}
+//}
+//
+//bool isPrime(int num) // 소수 판별
+//{
+//	if (num == 1) return false;
+//
+//	for (int i = 2; i <= sqrt(num); i++)
+//	{
+//		if (num % i == 0) return false;
+//	}
+//	return true;
+//}
+//
+//void findPartition(int num) // 파티션 계산&출력
+//{
+//	int result[3] = {0,};//i값, j값, 차이값 저장
+//
+//	for (int i = 1; i < num; i++)
+//	{
+//		for (int j = num - 1; j > 0; j--)
+//		{
+//			if (isPrime(i) == true && isPrime(j) == true)
+//			{
+//				if (i + j == num) {
+//					if (result[0] == 0) //첫 실행
+//					{
+//						result[0] = i;
+//						result[1] = j;
+//						result[2] = abs(i - j);
+//					}
+//					else if (result[2] > abs(i - j))
+//					{
+//						result[0] = i;
+//						result[1] = j;
+//						result[2] = abs(i - j);
+//					}
+//					else continue;
+//				}
+//			}
+//		}
+//	}
+//	cout << result[0] << " " << result[1] << endl;
+//}
