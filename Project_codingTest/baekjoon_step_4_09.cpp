f9f0c59bba15fde6d5a1374c0dab45c48ab378c8
woobba94 <66201264+woobba94@users.noreply.github.com>
2021-06-23 12:46:16 +0900

    update

---
+++ Project_codingTest/baekjoon_step_4_09.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//using namespace std;
+//
+//bool isPrime(int num);
+//int main()
+//{
+//	int m, n;
+//
+//	cin >> m >> n;
+//
+//	for (int i = m; i <= n; i++) 
+//	{
+//		if (isPrime(i) == true) cout << i << endl;
+//	}
+//
+//}
+//
+//bool isPrime(int num)
+//{
+//	if (num == 1)return false;
+//	else
+//	{
+//		for (int i = 2; i <= sqrt(num); i++)
+//		{
+//			if (num % i == 0) return false;
+//		}
+//	}
+//	return true;
+//}
