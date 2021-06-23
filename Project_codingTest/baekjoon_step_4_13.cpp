a9d14638f8d72b1dd728ec384edc54e37ed19207
woobba94 <66201264+woobba94@users.noreply.github.com>
2021-06-23 16:52:09 +0900

    taxi clear

---
+++ Project_codingTest/baekjoon_step_4_13.cpp
@@ -0,0 +1,32 @@
+//#include <iostream>
+//#define _USE_MATH_DEFINES
+//#include <math.h>
+//using namespace std;
+//
+//double euclidCalculator(int r);
+//double taxiCalculator(int r);
+//int main()
+//{
+//	int R;
+//	cout << "반지름 입력 : ";
+//	cin >> R;
+//
+//	cout << fixed;
+//	cout.precision(6);
+//	cout << euclidCalculator(R) << endl;
+//	cout << taxiCalculator(R);
+//}
+//
+//double euclidCalculator(int r) //유클리드 기하학 계산
+//{
+//	double result;
+//	result = (double)r * r * M_PI;
+//	return result;
+//}
+//
+//double taxiCalculator(int r) // 택시 기하학 계산
+//{
+//	double result;
+//	result = (double)2 * r * r;
+//	return result;
+//}
