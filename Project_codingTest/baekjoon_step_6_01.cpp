22118a04d3abdf06984854d12d57ee4d267c9eac
woobba94 <66201264+woobba94@users.noreply.github.com>
2021-06-24 22:40:49 +0900

    temporary commit

---
+++ Project_codingTest/baekjoon_step_6_01.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int fibonacci(int n, int& zeroCount, int& oneCount);
+int main()
+{
+    int n, zeroCount = 0, oneCount = 0;
+    cin >> n;
+    vector<int> numbers(n, 0);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> numbers[i];
+    }
+
+}
+
+
+
+int fibonacci(int n, int &zeroCount, int &oneCount) 
+{
+    if (n == 0) 
+    {
+        printf("0");
+        zeroCount++;
+        return 0;
+    }
+    else if (n == 1) 
+    {
+        printf("1");
+        oneCount++;
+        return 1;
+    }
+    else 
+    {
+        return (fibonacci(n-1, zeroCount, oneCount)
+              + fibonacci(n-1, zeroCount, oneCount));
+    }
+}
