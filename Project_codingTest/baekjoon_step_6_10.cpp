87b0c8112b2d0c5bf1a72015ee3d69f5923499e4
woobba94 <jwj3199@naver.com>
2021-06-28 11:09:08 +0900

    bridge clear

--- Project_codingTest/baekjoon_step_6_10.cpp
+++ Project_codingTest/baekjoon_step_6_10.cpp
@@ -1,65 +1,40 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-
-int sigmaCal(int x);
-void fnc(vector <int>(&couple));
-
-int main()
-{
-	int caseCount = 0;
-	cin >> caseCount;
-	vector <vector<int>> couple(caseCount);
-
-	for (int i = 0; i < caseCount; i++)
-	{
-		cin >> couple[i][0] >> couple[i][1];
-	}
-	
-}
-
-void fnc(vector <vector<int>> (&couple))
-{
-	for (int i = 0; i < couple.size(); i++)
-	{
-		// n이 한개일때
-		if (couple[i][0] == 1)
-		{
-			cout << couple[i][1]; // 경우의 수 m개
-		}
-		// n이 두개
-		else if (couple[i][0] == 2)
-		{
-			sigmaCal(couple[i][1] - 1);
-		}
-		else // n은 3이상
-		{
-
-		}
-	}
-	
-}
-
-
-int sum(int x)
-{
-	int result = 0;
-	for (int i = 1; i <= x; i++)
-	{
-		result += sigmaCal(i);
-	}
-	return result;
-}
-
-// 시그마 연산 함수
-int sigmaCal(int x)
-{
-	int result = 0;
-	for (int i = x; i > 0; i--)
-	{
-		result += i;
-	}
-
-	return result;
-}
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int func(vector <vector<int>>(&bridge), int n, int m);
+//
+//int main()
+//{
+//	int caseCount = 0;
+//	cin >> caseCount;
+//	// 2차원 벡터 선언
+//	vector <vector<int>> bridge(caseCount);
+//
+//	int left, right;
+//
+//	while (caseCount--)
+//	{
+//		cin >> left >> right;
+//		// 2차원 벡터 초기화
+//		vector<vector<int>> bridge(left + 1, vector<int>(right + 1, -1));
+//		cout << func(bridge, left, right) << endl;;
+//	}
+//}
+//
+//int func(vector <vector<int>> (&bridge), int n, int m)
+//{
+//	if (n == 1) {
+//		if (bridge[n][m] == -1) {
+//			bridge[n][m] = m; // 초기화 했기 때문에 사용가능
+//		}
+//		return bridge[n][m];
+//	}
+//	if (bridge[n][m] == -1) {
+//		bridge[n][m] = 0;
+//		for (int i = 1; i <= m - n + 1; ++i) {
+//			bridge[n][m] += func(bridge, n - 1, m - i);
+//		}
+//	}
+//	return bridge[n][m];
+//}
