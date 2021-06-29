70de5cc86bf9476bc92f70f233c205afcc6cf49c
woobba94 <66201264+woobba94@users.noreply.github.com>
2021-06-29 21:02:51 +0900

    nm03 clear

---
+++ Project_codingTest/baekjoon_step_8_03.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//void func(int n, int m, vector <int> vec, int cnt);
+//int main() 
+//{
+//    ios_base::sync_with_stdio(0);
+//    cin.tie(0);
+//
+//    int n, m;
+//    vector <int> vec(9, 0);
+//
+//    cin >> n >> m;
+//
+//    func(n, m, vec, 0);
+//
+//}
+//
+//void func(int n, int m, vector <int> vec, int cnt)
+//{
+//    if (cnt == m) 
+//    {
+//        for (int i = 0; i < m; i++) 
+//        {
+//            cout << vec[i] << " ";
+//        }
+//        cout << "\n";
+//        return;
+//    }
+//
+//    for (int i = 1; i <= n; i++) 
+//    {
+//        vec[cnt] = i;
+//        func(n, m, vec, cnt + 1);
+//    }
+//}
