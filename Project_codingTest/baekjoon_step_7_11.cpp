78cbfa8ef8d9bae0ff2dc5899284ccf107da6042
woobba94 <66201264+woobba94@users.noreply.github.com>
2021-06-29 19:09:05 +0900

    line taxi clear

---
+++ Project_codingTest/baekjoon_step_7_11.cpp
@@ -0,0 +1,48 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	long long m = 0;
+//
+//	cin >> n >> m;
+//
+//	vector <pair<long long, long long>> vec(n);
+//
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		long long start, end;
+//		cin >> start >> end;
+//
+//		// 역방향 손님들만 저장
+//		if (start > end)vec.push_back({ end, start });
+//	}
+//
+//	sort(vec.begin(), vec.end());
+//
+//	long long from = vec[0].first;
+//	long long to = vec[0].second;
+//
+//	for (int i = 0; i < vec.size(); i++)
+//	{
+//		// 저장된 목적지보다 새로운 출발지가 더 크면
+//		if (to < vec[i].first)
+//		{
+//			m += (to - from) * 2;
+//			from = vec[i].first;
+//			to = vec[i].second;
+//		}
+//		// 저장된 목적지보다 새로운 목적지가 더 크면
+//		else if (to < vec[i].second)
+//		{
+//			to = vec[i].second; // 목적지 갱신
+//		}
+//	}
+//	m += 2 * (to - from);
+//	cout << m;
+//
+//}
