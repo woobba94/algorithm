fc428b9a59a926ce7114f939dfedd58ac7aeec3e
woobba94 <jwj3199@naver.com>
2021-06-29 13:27:41 +0900

    matrix clear

---
+++ enc_temp_folder/941878326ce95973e8e59524f28ffd/baekjoon_step_7_08.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAX_SIZE = 100;
+void matrixCal(vector <vector <int>>& matrixA, vector <vector <int>>& matrixB, vector <vector <int>>& result, int N, int M, int K);
+
+int main()
+{
+    vector <vector <int>> matrixA(MAX_SIZE);
+    vector <vector <int>> matrixB(MAX_SIZE);
+    vector <vector <int>> result(MAX_SIZE);
+    
+    int N, M, K; // a(n, m), b(m, k)
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            int input = 0;
+            cin >> input;
+            matrixA[i].push_back(input);
+        }
+    }
+    cin >> M >> K;
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < K; j++)
+        {
+            int input = 0;
+            cin >> input;
+            matrixB[i].push_back(input);
+        }
+    }
+    matrixCal(matrixA, matrixB, result, N, M, K);
+}
+
+void matrixCal(vector <vector <int>>& matrixA, vector <vector <int>>& matrixB, vector <vector <int>> &result, int N, int M, int K)
+{
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < K; j++)
+        {
+            for (int k = 0; k < M; k++)
+            {
+                result[i][j] += (matrixA[i][k] * matrixB[k][j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < K; j++)
+        {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+}
