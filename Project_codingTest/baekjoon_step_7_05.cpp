43cbad4a7ecbf320babad59eb9842ee9dbd7d62e
woobba94 <jwj3199@naver.com>
2021-06-29 12:33:45 +0900

    c

---
+++ Project_codingTest/baekjoon_step_7_05.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+const int DIVISOR = 1000000007;
+int pactorial(int num);
+int cal(int n, int k);
+int main()
+{
+	int n = 0, k = 0;
+
+	cin >> n >> k;
+
+	cout << cal(n, k) % DIVISOR;
+}
+
+int cal(int n, int k)
+{
+	if (k < 0) return 0;
+	if (n < k) return 0;
+
+	return pactorial(n) / (pactorial(k) * pactorial(n - k));
+}
+
+int pactorial(int num)
+{
+	if (num < 0) return 0;
+	if (num == 1) return 1;
+
+	return num * pactorial(num - 1);
+}
