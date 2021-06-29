77bbb379156630df6dc83cd78e35c590b79a46c5
woobba94 <66201264+woobba94@users.noreply.github.com>
2021-06-29 20:56:03 +0900

    nm02 clear

---
+++ Project_codingTest/baekjoon_step_8_02.cpp
@@ -0,0 +1,44 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//void func(int n, int m, vector <int> vec, vector <bool> isVisited, int cnt, int newNum);
+//
+//int main()
+//{
+//    ios_base::sync_with_stdio(0);
+//    cin.tie(0);
+//
+//    int n, m;
+//    vector <int> vec(9, 0);
+//    vector <bool> isVisited(9, 0);
+//
+//    cin >> n >> m;
+//
+//    func(n, m, vec, isVisited, 0, 1);
+//
+//}
+//
+//void func(int n, int m, vector <int> vec, vector <bool> isVisited, int cnt, int newNum)
+//{
+//    if (cnt == m)
+//    {
+//        for (int i = 0; i < m; i++)
+//        {
+//            cout << vec[i] << " ";
+//        }
+//        cout << "\n";
+//        return;
+//    }
+//
+//    for (int i = newNum; i <= n; i++)
+//    {
+//        if (isVisited[i] == false)
+//        {
+//            isVisited[i] = true;
+//            vec[cnt] = i;
+//            func(n, m, vec, isVisited, cnt + 1, i + 1);
+//            isVisited[i] = false;
+//        }
+//    }
+//}
