ad05226ca763556ebc3aff65ec42697ef3cd23cd
woobba94 <jwj3199@naver.com>
2021-06-30 12:51:50 +0900

    stack02 clear

--- Project_codingTest/baekjoon_step_9_02.cpp
+++ Project_codingTest/baekjoon_step_9_02.cpp
@@ -1,77 +1,52 @@
-#include <iostream>
-#include <stack>
-#include <string>
-
-using namespace std;
-
-void func(stack <int> st, string str, int n);
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int n;
-    cin >> n;
-
-    stack<int> st; // 정수형 stack 선언. 
-    string str;
-
-    func(st, str, n);
-}
-
-void func(stack <int> st, string str, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cin >> str;
-        //push 명령어 일때. 
-        if (str == "push")
-        {
-            int num;
-            cin >> num;
-            st.push(num);
-        }
-        //pop 명령어 일때. 
-        else if (str == "pop")
-        {
-            if (!st.empty())
-            {
-                cout << st.top() << endl;
-                st.pop();
-            }
-            else
-            {
-                cout << "-1" << endl;
-            }
-        }
-        //size 명령어 일때
-        else if (str == "size")
-        {
-            cout << st.size() << endl;
-        }
-        //empty 명령어 일때. 
-        else if (str == "empty")
-        {
-            if (st.empty())
-            {
-                cout << "1" << endl;
-            }
-            else
-            {
-                cout << "0" << endl;
-            }
-        }
-        //top 명령어 일때. 
-        else if (str == "top")
-        {
-            if (!st.empty())
-            {
-                cout << st.top() << endl;
-            }
-            else
-            {
-                cout << "-1" << endl;
-            }
-        }
-    }
-}
+//#include <iostream>
+//#include <stack>
+//
+//using namespace std;
+//
+//void func(stack <int> st, int input, int n);
+//
+//int main()
+//{
+//    ios_base::sync_with_stdio(0);
+//    cin.tie(0);
+//
+//    int n;
+//    cin >> n;
+//
+//    stack<int> st; // 정수형 stack 선언. 
+//    int input = 0;
+//
+//    func(st, input, n);
+//}
+//
+//void func(stack <int> st, int input, int n)
+//{
+//    for (int i = 0; i < n; i++)
+//    {
+//        cin >> input;
+//        // 0 입력 -> 팝
+//        if (input == 0)
+//        {
+//            if (st.empty() == false) //지울 수 있는 수가 있음을 보장
+//            {
+//                st.pop();
+//            }
+//            else cout << "지울 수 있는 수가 없음. \n";
+//            
+//        }
+//        // 0 이외의 수 
+//        else
+//        {
+//            st.push(input);
+//        }
+//    }
+//    int count = st.size();
+//    int result = 0;
+//    for (int i = 0; i < count; i++)
+//    {
+//        result += st.top();
+//        st.pop();
+//    }
+//
+//    cout << result;
+//}
