0f2265b311f3d9a428203a0700c098cd3e6b35eb
woobba94 <jwj3199@naver.com>
2021-07-01 18:01:14 +0900

    deque01 clear

--- Project_codingTest/baekjoon_step_9_10.cpp
+++ Project_codingTest/baekjoon_step_9_10.cpp
@@ -1,52 +1,104 @@
 //#include <iostream>
 //#include <deque>
-//#include <vector>
-//#include <string>
 //
 //using namespace std;
 //
-//void func(deque <int> que, int n, int k);
+//void func(deque <pair<int, int>> que, int n, int m);
+//int findMin(deque <pair<int, int>> que);
+//int findMax(deque <pair<int, int>> que);
 //int main()
 //{
 //	ios_base::sync_with_stdio(0);
 //	cin.tie(0);
 //
-//    int n, k;
-//    cin >> n >> k;
+//	int caseCount, n, m;
+//    cin >> caseCount;
+//    deque <pair<int, int>> que; // 정수 pair 형 큐 선언. 
 //
-//    deque<int> que; // 정수형 큐 선언. 
+//	for (int i = 0; i < caseCount; i++)
+//	{
+//		cin >> n >> m;
+//		func(que, n, m);
+//	}
+//}
 //
-//    for (int i = 1; i <= n; i++)
-//    {
-//        que.push_back(i);
-//    }
-//    cout << "<";
-//    func(que, n, k);
-//    cout << ">";
+//void func(deque <pair<int, int>> que, int n, int m)
+//{
+//	int result = 0;
+//	for (int j = 0; j < n; j++)
+//	{
+//		int tmp;
+//		cin >> tmp;
+//		pair<int, int> p(tmp, j);
+//		que.push_back(p);
+//	}
+//	int min = findMin(que);
+//	int max = findMax(que);
+//	int count = 0;
+//	while (true) 
+//	{
+//		// 최솟값과 최댓값이 같으면 우선순위 동일
+//		if (min == max) break;
+//
+//		// 임시 변수에 첫번째 큐 저장 
+//		pair <int, int> tmp = que.front();
+//		// 첫번째 큐 팝
+//		que.pop_front();
+//		// 변수의 우선순위가 최댓값
+//		if (tmp.first == max)
+//		{
+//			count++;
+//			// 최댓값 갱신
+//			max = findMax(que);
+//			// 우선순위 최대면서 인덱스가 m이면
+//			if (tmp.second == m)
+//			{
+//				result = count;
+//			}
+//		}
+//		else 
+//		{
+//			que.push_back(tmp);
+//		}
+//	}
+//	for (int j = 0; j < que.size(); j++)
+//	{
+//		count++;
+//		int tmp = que.at(j).second;
+//		// j번째 우선순위의 인덱스가 m이랑 같으면 인쇄
+//		if (tmp == m) {
+//			result = count;
+//			break;
+//		}
+//	}
+//	cout << result << endl;
 //    
 //}
 //
-//void func(deque <int> que, int n, int k)
+//int findMin(deque <pair<int, int>> que) 
 //{
-//    while (true)
-//    {
-//        // 큐가 비어있으면 리턴
-//        if (que.empty() == true) return;
+//	int min = 10;
+//	for (int i = 0; i < que.size(); i++) 
+//	{
+//		int tmp = que.at(i).first;
+//		if (tmp <= min) 
+//		{
+//			min = tmp;
+//		}
+//	}
+//	return min;
+//}
 //
-//        for (int i = 0; i < k - 1; i++) 
-//        {
-//            que.push_back(que.front()); // 젤 앞에거 맨뒤로 푸시
-//            que.pop_front(); // 젤 앞에거 팝
-//        }
-//
-//        cout << que.front();
-//        que.pop_front();
-//
-//        // 마지막 원소가 아니면
-//        if (que.empty() == false)
-//        {
-//            cout << ", ";
-//        }
-//
-//    }
+//int findMax( deque <pair<int, int>> que) 
+//{
+//	int max = 0;
+//	for (int i = 0; i < que.size(); i++) 
+//	{
+//		int tmp = que.at(i).first;
+//		if (tmp >= max) 
+//		{
+//			max = tmp;
+//		}
+//	}
+//	return max;
 //}
