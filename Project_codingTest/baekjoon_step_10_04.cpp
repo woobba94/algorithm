a01b503e44429028e2ad1e2243c4e0fcc0edb803
woobba94 <jwj3199@naver.com>
2021-07-02 13:05:03 +0900

    commit

--- Project_codingTest/baekjoon_step_10_04.cpp
+++ Project_codingTest/baekjoon_step_10_04.cpp
@@ -1,64 +1,56 @@
-#include <stdio.h>
-#include <iostream>
-#include <queue>
-#include <vector>
-
-using namespace std;
-
-
-struct comp {
-    bool operator()(int x, int y)
-    {
-        if (abs(x) == abs(y)) // 절댓값 같으면 
-        {
-            return x > y;
-        }
-        else
-        {
-            return abs(x) > abs(y);
-        }
-    }
-};
-void func(priority_queue<int, vector<int>, comp>& que, int num);
-
-int main() 
-{
-    
-
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    priority_queue <int, vector<int>, comp> que; // int형 우선순위 큐 선언
-    int n;
-    cin >> n;
-    while (n--) 
-    { 
-        int x;
-        cin >> x;
-        func(que, x);
-    }
-    return 0;
-}
-
-void func(priority_queue <int, vector<int>, comp> &que, int num)
-{
-    if (num == 0) // 최댓값 출력 명령
-    {
-        // 비어있을 경우 0
-        if (que.empty())
-        {
-            cout << 0 << endl;
-        }
-        // 아닐경우 가장 위 출력, pop
-        else
-        {
-            cout << que.top() << endl;
-            que.pop();
-        }
-    }
-    // 입력 명령
-    else
-    {
-        que.push(num);
-    }
-}
+//#include <iostream>
+//#include <queue>
+//#include <vector>
+//
+//using namespace std;
+//
+//void func(vector <int> vec, priority_queue <int> queA, priority_queue <int> queB);
+//
+//int main(void)
+//{
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    vector <int> vec;
+//    // 중간값 구하기위해 우선순위큐 두개 선언
+//    priority_queue <int> queA, queB;
+//    int n;
+//    cin >> n;
+//    for (int i = 0; i < n; i++)
+//    {
+//        int temp;
+//        cin >> temp;
+//        vec.push_back(temp);
+//    }
+//
+//    func(vec, queA, queB);
+//
+//}
+//
+//void func(vector <int> vec, priority_queue <int> queA, priority_queue <int> queB)
+//{
+//    int valA = 0, valB = 0;
+//    for (int i = 0; i < vec.size(); i++)
+//    {
+//        if (queA.size() > queB.size()) queB.push(-vec[i]);
+//        else queA.push(vec[i]);
+//
+//        // 두 큐가 비어있지 않으면
+//        if (queA.empty() == false && queB.empty() == false)
+//        {
+//            // 최댓값의 탑이 -최솟값탑보다 크면
+//            if (queA.top() > -queB.top())
+//            {
+//                // 각 val 갱신
+//                valA = queA.top();
+//                valB = -queB.top();
+//
+//                queA.pop();
+//                queB.pop();
+//
+//                queA.push(valB);
+//                queB.push(-valA);
+//            }
+//        }
+//        cout << queA.top() << endl;
+//    }
+//}
