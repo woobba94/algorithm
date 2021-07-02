842eb05877d63cdd0a72e091c52c351ffc0f2411
woobba94 <jwj3199@naver.com>
2021-07-02 16:28:52 +0900

    c

---
+++ Project_codingTest/baekjoon_step_11_03.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+void func(int num, int x, deque <int> que, int& count);
+void frontCheck(int x, deque <int>& que);
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int a = 64;
+	int x;
+	deque <int> que;
+	cin >> x;
+	int temp = a;
+	int count = 0;
+
+	while (1)
+	{
+		que.push_back(temp);
+		if (temp == 1) break;
+		temp /= 2;
+	}
+
+	func(a, x, que, count);
+	cout << count << endl;
+
+	return 0;
+}
+
+void calc(int num, int x, deque <int> que, int& count)
+{
+	int size = que.size();
+
+	for (int i = 0; i < que.size() - 1; i++)
+	{
+		frontCheck(x, que);
+	}
+
+	int sum = 0;
+
+	while(1)
+	{
+		if (sum == x) return;
+		sum += que.front();
+		if (sum > x)
+		{
+			sum -= que.front();
+		}
+		else count++;
+		que.pop_front();
+	}
+}
+
+void frontCheck(int x, deque <int>& que)
+{
+	// x 보다 큰수 다 날림
+	if (que[0] > x)
+	{
+		que.pop_front();
+	}
+}
