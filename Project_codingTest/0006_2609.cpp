7264f5a614abe61952c22715d51b9106218c5949
woobba94 <jwj3199@naver.com>
2021-07-04 22:36:47 +0900

    2609 clear (GCD, LCM)

---
+++ Project_codingTest/0006_2609.cpp
@@ -0,0 +1,39 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int getGCD(int, int);
+//int getLCM(int, int, int gcd);
+//int main()
+//{
+//	int num1, num2;
+//
+//	cin >> num1 >> num2;
+//
+//
+//	if (num2 > num1)
+//	{
+//		int temp = num1;
+//		num1 = num2;
+//		num2 = temp;
+//	}
+//
+//	cout << getGCD(num1, num2) << endl;
+//
+//	cout << getLCM(num1, num2, getGCD(num1, num2));
+//}
+//
+//// 최대공약수 (GCD : greatest common divisor)
+//int getGCD(int num1, int num2) 
+//{
+//	if (num2 == 0)
+//		return num1;
+//
+//	return getGCD(num2, num1 % num2);
+//}
+//
+//// 최소공배수 (LCM : lowest common multiple)
+//int getLCM(int num1, int num2, int gcd)
+//{
+//	return num1 * num2 / gcd;
+//}
