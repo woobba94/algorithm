72fcf6414dacb5d0f3f55c991dc56c323690d01a
woobba94 <jwj3199@naver.com>
2021-07-05 21:51:24 +0900

    0017 clear

---
+++ Project_codingTest/0017_1476.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//
+//using namespace std;
+//void calc(int e, int s, int m);
+//int main()
+//{
+//	int E, S, M;
+//	cin >> E >> S >> M;
+//	calc(E, S, M);
+//}
+//
+//void calc(int e, int s, int m)
+//{
+//	int year = 1;
+//	while (true)
+//	{
+//		if (e == 1 && s == 1 && m == 1)
+//		{
+//			cout << year;
+//			return;
+//		}
+//		e--; s--; m--; year++;
+//		if (e == 0) e = 15;
+//		if (s == 0) s = 28;
+//		if (m == 0) m = 19;
+//	}
+//}
