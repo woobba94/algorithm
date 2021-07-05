e651ae286ee6bfc1b9bb1ff51ff17feb01020fe6
woobba94 <jwj3199@naver.com>
2021-07-05 16:36:14 +0900

    0013 clear

---
+++ Project_codingTest/0013_11653.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//
+//using namespace std;
+//void calc(int n);
+//int main()
+//{
+//	int n = 0;
+//
+//	cin >> n;
+//
+//	calc(n);
+//}
+//
+//// 소인수분해 함수
+//void calc(int n)
+//{
+//	int size = n;
+//	if (n < 2) return;
+//	for (int i = 2; i <= size; i++)
+//	{
+//		if (n % i == 0)
+//		{
+//			cout << i << endl;
+//			n /= i;
+//			i--;
+//		}
+//	}
+//}
