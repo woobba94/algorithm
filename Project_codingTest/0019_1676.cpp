2eb39975635bf444d226e3f82cbf0dabd8669f71
woobba94 <jwj3199@naver.com>
2021-07-06 12:45:25 +0900

    0019 clear

---
+++ Project_codingTest/0019_1676.cpp
@@ -0,0 +1,36 @@
+//#include <iostream>
+//using namespace std;
+//
+//int calc(int num);
+//int main()
+//{
+//	int num = 0, i = 0;
+//
+//	cin >> num;
+//
+//	cout << calc(num);
+//}
+//
+//
+//int calc(int num)
+//{
+//	long long bigNum = 1;
+//	int result = 0;
+//	// 팩도리얼 계산 X
+//	for (int i = 2; i < num + 1; i++)
+//	{
+//		// i 검사
+//		int temp = i;
+//		while (true)
+//		{
+//			if (temp % 5 == 0)
+//			{
+//				result++;
+//				temp /= 5;
+//			}
+//			else break;
+//		}
+//		
+//	}
+//	return result;
+//}
