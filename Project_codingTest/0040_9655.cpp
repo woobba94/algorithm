1152bde700d4650be4b70ef46340394f89a8d8d3
woobba94 <jwj3199@naver.com>
2021-07-09 13:03:22 +0900

    0040 clear

---
+++ Project_codingTest/0040_9655.cpp
@@ -0,0 +1,21 @@
+//#include <iostream> 
+//using namespace std; 
+//
+////bool isEven(long long num);
+//int main() 
+//{ 	int n; 
+//	cin >> n; 
+//	if (n % 2)
+//	{
+//		cout << "SK" << endl;
+//	}
+//	else
+//	{
+//		cout << "CY" << endl;
+//	}
+//}
+//// Â¦¼ö ÆÇº°
+//bool isEven(int num)
+//{
+//	return num % 2 == 0 ? true : false;
+//}
