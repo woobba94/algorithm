9021cf2a8c6fe3a6406840a63717585ea609a409
woobba94 <jwj3199@naver.com>
2021-07-13 17:17:57 +0900

    0061 clear

---
+++ Project_codingTest/0061_11720.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	char num;
+//	int sum = 0;
+//	int n = 0;
+//	cin >> n;
+//
+//	while (n--)
+//	{
+//		
+//		cin >> num;
+//		sum += num - '0';
+//	}
+//
+//	cout << sum;
+//}
