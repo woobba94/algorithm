e424617bf715331ca344278bdfd0e3bee828ba55
woobba94 <jwj3199@naver.com>
2021-07-14 12:24:05 +0900

    0067 clear

---
+++ Project_codingTest/0067_10870.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int fibo(int num);
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//
+//	cout << fibo(n);
+//}
+//
+//int fibo(int num)
+//{
+//	if (num == 0) return 0;
+//	if (num == 1) return 1;
+//
+//	return fibo(num - 1) + fibo(num - 2);
+//}
