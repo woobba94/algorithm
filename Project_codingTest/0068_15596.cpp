6aac93a5c405c87b55b3146c3b203a36304467f2
woobba94 <jwj3199@naver.com>
2021-07-14 13:01:25 +0900

    0068 clear

---
+++ Project_codingTest/0068_15596.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//
+//using namespace std;
+//long long sum(int* a, int n);
+//int main()
+//{
+//	// 0 ¡Â a[i] ¡Â 1,000,000
+//	// 
+//	// 1 ¡Â n ¡Â 3,000,000
+//
+//	int n;
+//	cin >> n;
+//
+//	int* arr = new int[n];
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//	cout << sum(arr, n);
+//
+//}
+//
+//long long sum(int* a, int n)
+//{
+//	long long sum = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		sum += a[i];
+//	}
+//
+//	return sum;
+//}
