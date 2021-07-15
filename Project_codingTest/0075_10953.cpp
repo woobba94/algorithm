cb2c49f00f5dbf5a1198994cdd232045d96d39cf
woobba94 <jwj3199@naver.com>
2021-07-15 18:59:39 +0900

    0075 clear

---
+++ Project_codingTest/0075_10953.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int testCase = 0;
+//	char numCommaNum[4] = { ' ', };
+//
+//	cin >> testCase;
+//
+//	while (testCase--)
+//	{
+//		
+//		cin >> numCommaNum;
+//
+//		int sum = int(numCommaNum[0] - '0') + int(numCommaNum[2] - '0');
+//
+//		cout << sum << endl;
+//	}
+//}
