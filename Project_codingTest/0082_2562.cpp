17e988ccc5e5ef3768ac1cc857e11022aa9f855f
woobba94 <jwj3199@naver.com>
2021-07-16 16:44:11 +0900

    2562 clear

---
+++ Project_codingTest/0082_2562.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int arr[9] = {};
+//
+//	for (int i = 0; i < 9; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//
+//	int index = 0;
+//	int max = 0;
+//
+//	for (int i = 0; i < 9; i++)
+//	{
+//		if (max < arr[i])
+//		{
+//			index = i;
+//			max = arr[i];
+//		}
+//	}
+//	cout << max << endl;
+//	cout << index + 1;
+//}
