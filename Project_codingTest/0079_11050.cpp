1cab3be1a5874ee4a80e97953eacc2be8ae10225
woobba94 <jwj3199@naver.com>
2021-07-16 10:25:25 +0900

    0079 clear

---
+++ Project_codingTest/0079_11050.cpp
@@ -0,0 +1,30 @@
+//#include<iostream>
+//using namespace std;
+//
+//int calc(int num);
+//
+//int main(void) 
+//{
+//
+//    int n, k;
+//
+//    cin >> n >> k;
+//
+//    cout << calc(n) / (calc(k) * calc(n - k));
+//
+//}
+//
+//
+//int calc(int num)
+//{
+//    int result = 1;
+//
+//    if (num == 0) return 1;    
+//
+//    for (int i = num; i >= 1; i--) 
+//    {
+//        result *= i;
+//    }
+//
+//    return result;
+//}
