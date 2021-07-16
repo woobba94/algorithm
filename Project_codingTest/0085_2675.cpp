b0b51efa440dfe8a0b9732549352d952f8a1757e
woobba94 <jwj3199@naver.com>
2021-07-16 20:25:07 +0900

    0085 clear

---
+++ Project_codingTest/0085_2675.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int testCase = 0;
+//	cin >> testCase;
+//
+//	while (testCase--)
+//	{
+//		int R = 0;
+//		char string[21] = {};
+//
+//		cin >> R;
+//		cin >> string;
+//
+//		for (int i = 0; i < 21; i++)
+//		{
+//			if (string[i] == '\0') break;
+//
+//			for(int j = 0; j < R; j++)
+//			{
+//				cout << string[i];
+//			}
+//		}
+//		cout << endl;
+//
+//	}
+//}
