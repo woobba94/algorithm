09b92f88bf591b3cfce373110b29a2aa87fe4f81
woobba94 <jwj3199@naver.com>
2021-07-18 13:32:29 +0900

    0094 clear

---
+++ Project_codingTest/0094_11721.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	char str[101]; // 최대길이 100
+//
+//	cin >> str;
+//
+//	// 10개씩 끊어서 출력
+//	for(int i = 0 ; i < 101; i++)
+//	{
+//		if (str[i] == '\0') break;
+//
+//		cout << str[i];
+//
+//		if (1 < i)
+//		{
+//			if ((i + 1) % 10 == 0) cout << endl;
+//		}
+//		
+//	}
+//}
