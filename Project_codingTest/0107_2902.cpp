3e8babac8c7b603f484495e38253b78bcf5592f8
woobba94 <jwj3199@naver.com>
2021-07-19 19:48:34 +0900

    0107 clear

---
+++ Project_codingTest/0107_2902.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	char input[101] = {};
+//
+//	cin >> input;
+//
+//	cout << input[0];
+//	for (int i = 0; i < 100; i++)
+//	{
+//		if (input[i] == '\0')break;
+//		if (input[i] == '-') cout << input[i + 1];
+//	}
+//}
