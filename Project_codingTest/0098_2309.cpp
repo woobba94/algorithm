a662fe5f3d4bf32fd6d45cfa1e2272bc664e1058
woobba94 <66201264+woobba94@users.noreply.github.com>
2021-07-19 13:07:18 +0900

    0098 init

---
+++ Project_codingTest/0098_2309.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[9];
+
+	for (int i = 0; i < 9; i++)
+	{
+		cin >> arr[i];
+	}
+
+	// 먼저 모든 키를 더함
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			
+		}
+	}
+}
