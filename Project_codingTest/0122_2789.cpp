638dd6a6e6eb1c0a1337e0a0d0b6f431536f9e97
woobba94 <jwj3199@naver.com>
2021-07-20 21:08:59 +0900

    0122 clear

---
+++ Project_codingTest/0122_2789.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	char arr[10] = { 'C','A','M','B','R','I','D','G','E' };
+//	char input[101] = {};
+//
+//	cin >> input;
+//
+//	for (int i = 0; i < 100; i++)
+//	{
+//		if (input[i] == '\0') break;
+//		for (int j = 0; j < 9; j++)
+//		{
+//			if (input[i] == arr[j]) input[i] = '0';
+//		}
+//
+//		if (input[i] != '0')
+//			cout << input[i];
+//	}
+//}
