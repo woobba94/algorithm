29c6729833c50441aba4454454283ae62d0e4b64
woobba94 <jwj3199@naver.com>
2021-07-20 21:39:12 +0900

    0123 clear

---
+++ Project_codingTest/0123_9625.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int k;
+//	int a = 1;
+//	int b = 0;
+//	cin >> k;
+//
+//	while (k--)
+//	{
+//		int temp = b;
+//
+//		b = b + a;
+//
+//		a = temp;
+//	}
+//
+//	cout << a << " " << b;
+//}
