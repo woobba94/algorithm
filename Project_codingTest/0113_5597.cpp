bdc6148d1de1778accb0cd057cb15a2e69792e74
woobba94 <jwj3199@naver.com>
2021-07-20 14:01:49 +0900

    0113 clear

---
+++ Project_codingTest/0113_5597.cpp
@@ -0,0 +1,33 @@
+//#include  <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int arr[28] = {};
+//
+//	for (int i = 0; i < 28; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//
+//	bool flag = false;
+//
+//	for (int i = 1; i < 31; i++)
+//	{
+//		for (int j = 0; j < 28; j++)
+//		{
+//			if (arr[j] == i)
+//			{
+//				flag = true;
+//				break;
+//			}
+//		}
+//		if (flag == false)
+//		{
+//			cout << i << endl;
+//		}
+//		flag = false;
+//	}
+//	
+//}
