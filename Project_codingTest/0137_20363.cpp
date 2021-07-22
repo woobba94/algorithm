7439a2d69fa0a18add66f2125340afbd5f98bd9c
woobba94 <jwj3199@naver.com>
2021-07-22 12:27:33 +0900

    0137 hold

--- Project_codingTest/0137_20363.cpp
+++ Project_codingTest/0137_20363.cpp
@@ -1,49 +1,46 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int result = 0;
-
-	int needWarm, needMoisture;
-	cin >> needWarm >> needMoisture;
-	
-	// 현재 온기와 수분
-	int nowWarm = 0;
-	int nowMoisture = 0;
-
-	int flag = false;
-	while (1)
-	{
-		if (needWarm == 0 && needMoisture == 0) break;
-
-		// 온기 꽉 채움
-		result += needWarm;
-		nowWarm += needWarm;
-
-		// 처음이면
-		if (flag == false)
-		{
-			flag = true;
-		}
-		else
-		{
-			nowMoisture -= needWarm / 10;
-			needMoisture += needWarm / 10;
-		}
-		needWarm = 0;
-		
-		// 수분 꽉 채움
-		result += needMoisture;
-		nowMoisture += needMoisture;
-
-		nowWarm -= needMoisture / 10;
-		needWarm += needMoisture / 10;
-
-		needMoisture = 0;
-
-	}
-
-	cout << result;
-
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int result = 0;
+//
+//	int needWarm, needMoisture;
+//	cin >> needWarm >> needMoisture;
+//
+//	if (needWarm < needMoisture)
+//	{
+//		int temp = needWarm;
+//		needWarm = needMoisture;
+//		needMoisture = temp;
+//	}
+//	
+//	// 현재 온기와 수분
+//	int nowWarm = 0;
+//	int nowMoisture = 0;
+//
+//	int flag = false;
+//	while (1)
+//	{
+//		if (needWarm == 0 && needMoisture == 0) break;
+//
+//		// 처음이면
+//		if (flag == false)
+//		{
+//			flag = true;
+//			result += needWarm;
+//			needWarm = 0;
+//			result += needMoisture - 1;
+//			result += (needMoisture - 1) / 10;
+//			needMoisture = 1;
+//		}
+//		else
+//		{
+//			result += needMoisture;
+//		}
+//			
+//	}
+//
+//	cout << result;
+//
+//}
