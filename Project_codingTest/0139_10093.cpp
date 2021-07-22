4c3964cc8809e8529f450e35b20bfb68766fc2f1
woobba94 <jwj3199@naver.com>
2021-07-22 14:39:55 +0900

    0139 clear

---
+++ Project_codingTest/0139_10093.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	long long a, b;
+//	cin >> a >> b;
+//
+//	if (a < b)
+//	{
+//		long long temp = a;
+//		a = b;
+//		b = temp;
+//	}
+//
+//	if (a != b)
+//	{
+//		cout << a - b - 1 << "\n";
+//
+//		for (long long i = b + 1; i < a; i++)
+//		{
+//			cout << i << " ";
+//		}
+//	}
+//	else cout << 0;
+//	
+//}
