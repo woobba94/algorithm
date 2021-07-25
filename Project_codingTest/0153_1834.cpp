09c9a59528158c59f74be5938f6771de3cf12e43
woobba94 <jwj3199@naver.com>
2021-07-25 18:38:11 +0900

    0153 clear

---
+++ Project_codingTest/0153_1834.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	long long n;
+//	cin >> n;
+//
+//	int k = 1;
+//	long long sum = 0;
+//
+//	// n 제곱 미만까지 검사
+//	while (k * (n + 1) < n * n)
+//	{
+//		sum += k * (n + 1);
+//
+//		k++;
+//	}
+//
+//	cout << sum;
+//}
