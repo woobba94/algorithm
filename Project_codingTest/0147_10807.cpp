7f14ada2aa2964ced918a57ec98bba7962757940
woobba94 <jwj3199@naver.com>
2021-07-25 16:38:27 +0900

    0147 clear

---
+++ Project_codingTest/0147_10807.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n; // N(1 ¡Â N ¡Â 100)
+//	cin >> n;
+//
+//	int* arr = new int[n];
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//
+//	int count = 0;
+//	int target;
+//	cin >> target;
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		if (arr[i] == target)
+//			count++;
+//	}
+//
+//	cout << count;
+//}
