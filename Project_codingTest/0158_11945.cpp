a59982ffbe32e3e4cd33542f778cddd3e06c2547
woobba94 <jwj3199@naver.com>
2021-07-25 21:11:55 +0900

    0158 clear

---
+++ Project_codingTest/0158_11945.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n, m; // (0≤N,M≤10)
+//	cin >> n >> m;
+//
+//	char arr[10][10] = {};
+//
+//	// 입력
+//	for (int i = 0; i < n; i++)
+//	{
+//		for (int j = 0; j < m; j++)
+//		{
+//			cin >> arr[i][j];
+//		}
+//	}
+//
+//	// 출력
+//	for (int i = 0; i < n; i++)
+//	{
+//		for (int j = m - 1; j >= 0; j--)
+//		{
+//			cout << arr[i][j];
+//		}
+//		cout << endl;
+//	}
+//
+//}
