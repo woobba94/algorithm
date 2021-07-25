433fd5e812dbbc44c45468172a4ca65ef794614c
woobba94 <jwj3199@naver.com>
2021-07-25 18:45:13 +0900

    0154 clear

---
+++ Project_codingTest/0154_1568.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//
+//	int i = 1;
+//
+//	int count = 0;
+//
+//	while (n)
+//	{
+//		count++;
+//		if (n < i)
+//		{
+//			i = 1;
+//		}
+//		n -= i;
+//		i++;
+//	}
+//	
+//	cout << count;
+//}
