05ab4b42ce862f0a1a03d47b10939721202e81e7
woobba94 <jwj3199@naver.com>
2021-07-30 17:34:03 +0900

    0180 clear

---
+++ Project_codingTest/0180_2979.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b, c;
+//	cin >> a >> b >> c;
+//
+//	int arr[100] = {};
+//	int start, end;
+//	for (int i = 0; i < 3; i++)
+//	{
+//		cin >> start >> end;
+//		for (int j = start; j < end; j++)
+//		{
+//			arr[j]++;
+//		}
+//	}
+//
+//	int cost = 0;
+//
+//	for (int i = 0; i < 100; i++)
+//	{
+//		if (arr[i] == 1) cost += a;
+//		else if (arr[i] == 2) cost += b * 2;
+//		else if (arr[i] == 3)cost += c * 3;
+//	}
+//
+//	cout << cost;
+//}
