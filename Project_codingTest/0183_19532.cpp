a61c7f9b3157446cd99d3fbf826e630440ea8c7e
woobba94 <jwj3199@naver.com>
2021-07-30 19:36:07 +0900

    0183 clear

---
+++ Project_codingTest/0183_19532.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b, c, d, e, f;
+//	cin >> a >> b >> c >> d >> e >> f;
+//
+//	int x = 0, y = 0;
+//
+//	for (int x = -999; x < 1000; x++)
+//	{
+//		for (int y = -999; y < 1000; y++)
+//		{
+//			if (a * x + b * y == c && d * x + e * y == f)
+//			{
+//				cout << x << " " << y;
+//				break;
+//			}
+//		}
+//	}
+//
+//}
