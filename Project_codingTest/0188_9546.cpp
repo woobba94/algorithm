d90bc0b38bf8b82d066cbbae5cfbd5795ca807e0
woobba94 <jwj3199@naver.com>
2021-07-30 20:39:01 +0900

    0188 clear

---
+++ Project_codingTest/0188_9546.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int k;
+//	double result = 1.0;
+//	while (n--)
+//	{
+//		cin >> k;
+//		// 마지막 정류장은 빼줌.
+//		k--;
+//		while (k--)
+//		{
+//			result = (result + 0.5) * 2;
+//		}
+//		
+//		cout << int(result) << endl;
+//		result = 1.0;
+//	}
+//}
