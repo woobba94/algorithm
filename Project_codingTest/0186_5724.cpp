27825b72366ca11268a74c124102a509f8fe129a
woobba94 <jwj3199@naver.com>
2021-07-30 20:08:34 +0900

    0186 clear

---
+++ Project_codingTest/0186_5724.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//using namespace std;
+//int dp[101] = {};
+//
+//int main()
+//{
+//	for (int i = 1; i <= 100; i++)
+//	{
+//		dp[i] = i * i + dp[i - 1];
+//	}
+//	int input = 0;
+//	while (1)
+//	{
+//		cin >> input;
+//		if (input == 0) break;
+//		cout << dp[input] << endl;
+//	}
+//}
