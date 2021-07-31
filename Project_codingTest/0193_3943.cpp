9f13e5caf1c2475db1ce3a5d3c686c9b6a2512d5
woobba94 <jwj3199@naver.com>
2021-07-31 18:02:07 +0900

    0193 clear

---
+++ Project_codingTest/0193_3943.cpp
@@ -0,0 +1,36 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
+//	int t;
+//	cin >> t;
+//	int n;
+//	while (t--)
+//	{
+//		cin >> n;
+//		int max = 1;
+//		while (n != 1)
+//		{
+//			
+//			if (max < n)
+//			{
+//				max = n;
+//			}
+//
+//			if (n % 2 == 0)
+//			{
+//				n /= 2;
+//			}
+//			else
+//			{
+//				n *= 3;
+//				n += 1;
+//			}
+//			
+//		}
+//		cout << max << '\n';
+//	}
+//}
