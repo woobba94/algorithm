811377785d7553f6b1eddc7d19787ae65eba00b4
woobba94 <jwj3199@naver.com>
2021-07-31 17:44:05 +0900

    0192 clear

---
+++ Project_codingTest/0192_1673.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//using namespace std;
+//int n, k;
+//void calc();
+//int main()
+//{
+//	while (cin >> n >> k)
+//	{
+//		calc();
+//	}
+//}
+//
+//void calc()
+//{
+//	int result = 0;
+//	int coupon = 0;
+//	while (1)
+//	{
+//		result += n;
+//		coupon += n;
+//		if (coupon < k)
+//			break;
+//		n = coupon / k;
+//		coupon = coupon % k;
+//	}
+//	cout << result << endl;
+//}
