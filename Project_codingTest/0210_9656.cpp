94d8e4912beb396c15c89b87a0f5af513a97bcb3
woobba94 <jwj3199@naver.com>
2021-08-04 15:52:36 +0900

    0210 clear

---
+++ Project_codingTest/0210_9656.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//
+//	if (n == 1)
+//		cout << "CY";
+//	else if (n == 3)
+//		cout << "CY";
+//	else if (n % 2 == 0)
+//	{
+//		cout << "SK";
+//	}
+//	else
+//	{
+//		cout << "CY";
+//	}
+//}
