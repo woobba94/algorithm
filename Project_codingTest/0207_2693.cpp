6ec24e2ab5cf362ffa7b1f34117c1ff79ffd5daa
woobba94 <jwj3199@naver.com>
2021-08-04 15:22:05 +0900

    0207 clear

---
+++ Project_codingTest/0207_2693.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//using namespace std;
+//void sort(int arr[]);
+//int main()
+//{
+//	int t;
+//	cin >> t;
+//	int arr[10] = {};
+//	while (t--)
+//	{
+//		for (int i = 0; i < 10; i++)
+//		{
+//			cin >> arr[i];
+//		}
+//		sort(arr);
+//
+//		cout << arr[2] << endl;
+//	}
+//}
+//
+//void sort(int arr[])
+//{
+//	for (int i = 0; i < 10; i++)
+//	{
+//		for (int j = 0; j < 9; j++)
+//		{
+//			if (arr[j] < arr[j + 1])
+//			{
+//				int temp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = temp;
+//			}
+//		}
+//	}
+//}
