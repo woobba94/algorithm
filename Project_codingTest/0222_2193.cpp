6f11c53096ef3c570d968b1fa51292dc7b260542
woobba94 <jwj3199@naver.com>
2021-08-06 13:01:47 +0900

    0222 clear

--- Project_codingTest/0222_2193.cpp
+++ Project_codingTest/0222_2193.cpp
@@ -1,7 +1,22 @@
 //#include <iostream>
 //using namespace std;
 //
+//long long dp[91] = {};
+//
 //int main()
 //{
+//	int n;
+//	cin >> n;
 //
+//
+//	dp[1] = 1;
+//	dp[2] = 1;
+//
+//	for (int i = 3; i <= n; i++)
+//	{
+//		dp[i] = dp[i - 1] + dp[i - 2];
+//	}
+//
+//
+//	cout << dp[n];
 //}
