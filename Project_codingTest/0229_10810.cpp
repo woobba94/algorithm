cc66c8f2338dd1c18782b27da7fa4db33e381ef7
woobba94 <jwj3199@naver.com>
2021-08-09 17:15:39 +0900

    0229 clear

---
+++ Project_codingTest/0229_10810.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n, m;
+//	cin >> n >> m;
+//
+//	int* box = new int [n + 1]();
+//
+//	int start, end, num;
+//
+//	while (m--)
+//	{
+//		cin >> start >> end >> num;
+//
+//		for (int i = start; i <= end; i++)
+//		{
+//			box[i] = num;
+//		}
+//	}
+//
+//	for (int i = 1; i < n + 1; i++)
+//	{
+//		cout << box[i] << " ";
+//	}
+//
+//}
