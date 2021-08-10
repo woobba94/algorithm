c18bf3f86c42d5b8145441de58b3f792d0f5aa43
woobba94 <jwj3199@naver.com>
2021-08-10 20:21:20 +0900

    0242 clear

---
+++ Project_codingTest/0242_1085.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//using namespace std;
+//
+//int getMin(int a, int b);
+//int main()
+//{
+//	int x, y, w, h;
+//
+//	cin >> x >> y >> w >> h;
+//
+//	cout << getMin(getMin(x, y), getMin(w - x, h - y));
+//}
+//
+//int getMin(int a, int b)
+//{
+//	if (a < b)
+//		return a;
+//	return b;
+//}
