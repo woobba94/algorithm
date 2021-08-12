b221f413c1f10022256e82d06e73a3fae2e03a8a
woobba94 <jwj3199@naver.com>
2021-08-12 17:30:44 +0900

    0246 clear

---
+++ Project_codingTest/0246_4470.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//using namespace std;
+//
+//const int maxSize = 51;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	cin.ignore();
+//
+//	char* input = new char[maxSize];
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin.getline(input, maxSize);
+//		cout << i + 1 << ". ";
+//		cout << input << endl;
+//	}
+//}
