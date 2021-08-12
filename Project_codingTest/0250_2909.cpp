a930c3ee81c372bc61fba91f362dfa4ca3f73360
woobba94 <jwj3199@naver.com>
2021-08-12 22:13:00 +0900

    0250 clear

---
+++ Project_codingTest/0250_2909.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int c;
+//	int n; // 0ÀÇ °³¼ö
+//	cin >> c >> n;
+//
+//	int money = 1;
+//	while (n--)
+//	{
+//		money *= 10;
+//	}
+//
+//	if (c % money > money / 2 - 1)
+//	{
+//		c = (c / money + 1) * money;
+//	}
+//	else
+//	{
+//		c = (c / money) * money;
+//	}
+//
+//	cout << c;
+//}
