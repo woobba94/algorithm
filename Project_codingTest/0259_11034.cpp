f122f5b7658e27ff23c569e34e9e8758add18d8a
woobba94 <jwj3199@naver.com>
2021-08-13 21:15:45 +0900

    0259 clear

---
+++ Project_codingTest/0259_11034.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+//
+//int getMax(int a, int b);
+//int main()
+//{
+//	int a, b, c;
+//
+//	while (cin >> a >> b >> c)
+//	{
+//		cout << getMax(b - a - 1, c - b - 1) << endl;
+//	}
+//}
+//
+//int getMax(int a, int b)
+//{
+//	if (a < b)
+//		return b;
+//	return a;
+//}
