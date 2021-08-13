d12675aefe6c42e6f0742c8dc045ba3c53ce454d
woobba94 <jwj3199@naver.com>
2021-08-13 15:42:30 +0900

    0215 clear

---
+++ Project_codingTest/0251_5347.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//using namespace std;
+//
+//int getGCD(long long a, long long b);
+//long long getLCM(long long a, long long b);
+//int main()
+//{
+//	int t;
+//	cin >> t;
+//	long long a, b;
+//	while (t--)
+//	{
+//		cin >> a >> b;
+//
+//		cout << getLCM(a, b) << endl;
+//	}
+//}
+//int getGCD(long long a, long long b)
+//{
+//	if (b == 0)
+//		return a;
+//	return getGCD(b, a % b);
+//}
+//
+//long long getLCM(long long a, long long b)
+//{
+//	return a * b / getGCD(a, b);
+//}
