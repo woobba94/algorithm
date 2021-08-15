22ff413693bec8097e3571a414256c56c5bea08e
woobba94 <jwj3199@naver.com>
2021-08-15 18:49:11 +0900

    0265 clear

---
+++ Project_codingTest/0265_21313.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	for (int i = 0; i < n - 1; i++)
+//	{
+//		if (i % 2)
+//			cout << 2 << " ";
+//		else
+//			cout << 1 << " ";
+//	}
+//	if (n % 2 != 0)
+//		cout << 3;
+//	else
+//		cout << 2;
+//}
