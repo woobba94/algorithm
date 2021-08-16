df884ecd85ae1ec7879a248112bde83fab4eaad3
woobba94 <jwj3199@naver.com>
2021-08-16 19:01:34 +0900

    0271 clear

---
+++ Project_codingTest/0271_1439.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//using namespace std;
+//
+//char arr[1000001];
+//int main()
+//{
+//	
+//	cin >> arr;
+//	int count = 0;
+//	for (int i = 0; i < 1000000; i++)
+//	{
+//		if (arr[i] == '\0')
+//		{
+//			break;
+//		}
+//
+//		if ((arr[i] == '0' && arr[i + 1] == '1') || (arr[i] == '1' && arr[i + 1] == '0'))
+//		{
+//			count++;
+//		}
+//	}
+//	if (count % 2 == 0)
+//		cout << count / 2;
+//	else
+//		cout << count / 2 + 1;
+//}
+//
