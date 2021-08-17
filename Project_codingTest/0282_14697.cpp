3609d7159224a30e2cc5deb9e2647a16bf33f3ae
woobba94 <jwj3199@naver.com>
2021-08-17 20:33:33 +0900

    0282 clear

---
+++ Project_codingTest/0282_14697.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b, c, n;
+//	cin >> a >> b >> c >> n;
+//	
+//	bool flag = false;
+//	for (int i = 0; i < 101; i++)
+//	{
+//		for (int j = 0; j < 101; j++)
+//		{
+//			for (int k = 0; k < 101; k++)
+//			{
+//				if (a * i + b * j + c * k == n)
+//				{
+//					flag = true;
+//					break;
+//				}
+//			}
+//		}
+//	}
+//	if (flag)
+//		cout << 1;
+//	else
+//		cout << 0;
+//}
