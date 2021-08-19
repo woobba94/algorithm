dff207048ca73e87d3219fc7e610cee39b3df8c9
woobba94 <jwj3199@naver.com>
2021-08-19 20:44:21 +0900

    0296 clear

---
+++ Project_codingTest/0296_2953.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int score = 0;
+//	int input;
+//	int max = 0;
+//	int maxIndex = 0;
+//	for (int i = 0; i < 5; i++)
+//	{
+//		for (int j = 0; j < 4; j++)
+//		{
+//			cin >> input;
+//			score += input;
+//		}
+//
+//		if (max < score)
+//		{
+//			max = score;
+//			maxIndex = i + 1;
+//		}
+//		score = 0;
+//	}
+//
+//	cout << maxIndex << " " << max;
+//}
