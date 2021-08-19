28081cda3c4607c7076b0992e17b48ec11c8a4c4
woobba94 <jwj3199@naver.com>
2021-08-19 15:00:56 +0900

    0289 clear

---
+++ Project_codingTest/0289_10871.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n, x;
+//	cin >> n >> x;
+//	int input;
+//	while (n--)
+//	{
+//		cin >> input;
+//		if (input < x)
+//			cout << input << " ";
+//	}
+//}
