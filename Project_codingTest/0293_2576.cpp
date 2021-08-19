ec8c1eb43bd6057cf9357ca1210c4d92d7cf0031
woobba94 <jwj3199@naver.com>
2021-08-19 19:55:27 +0900

    0293 clear

---
+++ Project_codingTest/0293_2576.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int num;
+//	int sum = 0;
+//	int min = 99;
+//	bool flag = true;
+//	for (int i = 0; i < 7; i++)
+//	{
+//		cin >> num;
+//		if (num % 2)
+//		{
+//			flag = false;
+//			sum += num;
+//			if (num < min)
+//			{
+//				min = num;
+//			}
+//		}
+//	}
+//
+//	if (flag)
+//	{
+//		cout << -1;
+//	}
+//	else
+//	{
+//		cout << sum << endl;
+//		cout << min;
+//	}
+//		
+//	
+//}
