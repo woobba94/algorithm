acd3844503736d39eb52e74707d009e5c39ba3d5
woobba94 <jwj3199@naver.com>
2021-08-19 19:47:10 +0900

    0292 clear

---
+++ Project_codingTest/0292_2455.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int in, out;
+//	int now = 0;
+//	int max = 0;
+//	for (int i = 0; i < 4; i++)
+//	{
+//		cin >> out >> in;
+//		now -= out;
+//		now += in;
+//		if (max < now)
+//		{
+//			max = now;
+//		}
+//	}
+//
+//	cout << max;
+//}
