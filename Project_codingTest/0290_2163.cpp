2b065b4225d0e622b3dd7af2ba37cfb2fb8f8b1d
woobba94 <jwj3199@naver.com>
2021-08-19 15:06:47 +0900

    0290 clear

---
+++ Project_codingTest/0290_2163.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n, m;
+//	cin >> n >> m;
+//	if (n < m)
+//	{
+//		int temp = n;
+//		n = m;
+//		m = temp;
+//	}
+//	cout << (m - 1) + (n - 1) * m;
+//}
