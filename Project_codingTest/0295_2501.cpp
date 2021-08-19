21e94f802ee881c770a00b4e38c81d06ea059a39
woobba94 <jwj3199@naver.com>
2021-08-19 20:35:15 +0900

    0295 clear

---
+++ Project_codingTest/0295_2501.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n, k;
+//	cin >> n >> k;
+//
+//	int num = 1;
+//	int count = 1;
+//	while (1)
+//	{
+//		if (n % num == 0)
+//		{
+//			if (count == k)
+//			{
+//				cout << num;
+//				break;
+//			}
+//			else
+//				count++;
+//		}
+//		num++;
+//		if (num > n)
+//		{
+//			cout << 0;
+//			break;
+//		}
+//	}
+//}
