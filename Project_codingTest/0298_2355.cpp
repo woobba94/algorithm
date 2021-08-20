8a4b95220c2ac78e82fdae7956916d4e7109a720
woobba94 <jwj3199@naver.com>
2021-08-20 19:56:26 +0900

    0298 clear

---
+++ Project_codingTest/0298_2355.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	long long a, b;
+//	cin >> a >> b;
+//	
+//	if (a > b)
+//	{
+//		int temp = a;
+//		a = b;
+//		b = temp;
+//	}
+//	cout << ((b * (b + 1)) / 2) - (((a -1) * (a)) / 2) << endl;
+//}
