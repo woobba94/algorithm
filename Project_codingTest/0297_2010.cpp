fce3667a73bb499c0a6f7e8891b1622220a7f054
woobba94 <jwj3199@naver.com>
2021-08-20 19:37:41 +0900

    0297 clear

---
+++ Project_codingTest/0297_2010.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int num = 0;
+//	int sum = 0;
+//	while (n--)
+//	{
+//		cin >> num;
+//		sum += num - 1;
+//	}
+//	cout << sum + 1;
+//}
