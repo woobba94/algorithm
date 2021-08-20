98f452f70b03a2680dbc9f1db4414eb54e2a15f0
woobba94 <jwj3199@naver.com>
2021-08-20 21:51:38 +0900

    0305 clear

---
+++ Project_codingTest/0305_10833.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int student, apple;
+//	int extraApple = 0;
+//	while (n--)
+//	{
+//		cin >> student >> apple;
+//		extraApple += apple % student;
+//	}
+//	cout << extraApple;
+//}
