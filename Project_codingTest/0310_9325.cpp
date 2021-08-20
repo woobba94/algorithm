7469053d8ef024e2af97b1d024c1eb0045a2d90a
woobba94 <jwj3199@naver.com>
2021-08-20 22:43:11 +0900

    0310 clear

---
+++ Project_codingTest/0310_9325.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int t;
+//	cin >> t;
+//	int s, n;
+//
+//	
+//	while (t--)
+//	{
+//		int result = 0;
+//		cin >> s;
+//		cin >> n;
+//		int q, p;
+//		while (n--)
+//		{
+//			cin >> q >> p;
+//			result += q * p;
+//		}
+//		result += s;
+//		cout << result << endl;
+//	}
+//}
