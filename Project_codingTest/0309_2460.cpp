da183aecc76006b718d6cf29ae97e43b5b381e8a
woobba94 <jwj3199@naver.com>
2021-08-20 22:37:45 +0900

    0309 clear

---
+++ Project_codingTest/0309_2460.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int t = 10;
+//	int in, out;
+//	int now = 0;
+//	int max = 0;
+//	while (t--)
+//	{
+//		cin >> out >> in;
+//		now -= out;
+//		now += in;
+//		if (max < now)
+//			max = now;
+//	}
+//	cout << max;
+//}
