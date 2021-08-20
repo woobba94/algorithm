e18db43866569a0dfcfd62545e1da353400f112e
woobba94 <jwj3199@naver.com>
2021-08-20 21:56:21 +0900

    0306 clear

---
+++ Project_codingTest/0306_2506.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	bool* arr = new bool[n];
+//	
+//	
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//
+//	int bonus = 1;
+//	int score = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		if (arr[i] == true)
+//		{
+//			score += bonus;
+//			bonus++;
+//		}
+//		else
+//			bonus = 1;
+//	}
+//	cout << score;
+//}
