dca6718dda3a35ffa9a4e3c62f32ff484b251531
woobba94 <jwj3199@naver.com>
2021-08-20 20:03:31 +0900

    0300 clear

---
+++ Project_codingTest/0300_5565.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int all, input;
+//	cin >> all;
+//	int sum = 0;
+//	int cycle = 9;
+//	while (cycle--)
+//	{
+//		cin >> input;
+//		sum += input;
+//	}
+//	cout << all - sum;
+//}
