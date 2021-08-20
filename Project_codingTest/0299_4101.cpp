8668f9913df2678561d8391a9a333c6d3f12aa9b
woobba94 <jwj3199@naver.com>
2021-08-20 20:00:11 +0900

    0299 clear

---
+++ Project_codingTest/0299_4101.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//using namespace std;
+//
+//bool isYes(int a, int b);
+//int main()
+//{
+//	int a, b;
+//	while (1)
+//	{
+//		cin >> a >> b;
+//		if (a == 0 && b == 0)
+//			break;
+//		if (isYes(a, b))
+//			cout << "Yes";
+//		else
+//			cout << "No";
+//		cout << endl;
+//	}
+//}
+//
+//bool isYes(int a, int b)
+//{
+//	if (a > b)
+//		return true;
+//	return false;
+//}
