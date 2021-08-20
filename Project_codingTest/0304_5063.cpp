8be649b37cbc0f42c156758e70cf96edd45f0d87
woobba94 <jwj3199@naver.com>
2021-08-20 21:44:22 +0900

    0304 clear

---
+++ Project_codingTest/0304_5063.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int t;
+//	cin >> t;
+//	while (t--)
+//	{
+//		int r, c, e;
+//		cin >> r >> c >> e;
+//		if (c - r > e)
+//			cout << "advertise";
+//		else if (c - r < e)
+//			cout << "do not advertise";
+//		else
+//			cout << "does not matter";
+//		cout << endl;
+//	}
+//}
