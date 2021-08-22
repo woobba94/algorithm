a75214a46932cb21c392e72e041262a35b2389fb
woobba94 <jwj3199@naver.com>
2021-08-22 21:29:13 +0900

    0315 clear

---
+++ Project_codingTest/0315_15719.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(0);
+//	cin.tie(0);
+//	int n;
+//	cin >> n;
+//	int num = 0;
+//	bool* isOverlap = new bool[n]();
+//
+//	int result = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> num;
+//		if (isOverlap[num]) 
+//		{
+//			cout << num;
+//			break;
+//		}
+//		else isOverlap[num] = true;
+//	}
+//}
