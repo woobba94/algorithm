6cf0510e63fb3f948110e2598419c3c6070c9818
woobba94 <jwj3199@naver.com>
2021-08-22 17:52:12 +0900

    0311 clear

---
+++ Project_codingTest/0311_10886.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	bool isCute;
+//	int result = 0;
+//	while (n--)
+//	{
+//		cin >> isCute;
+//		if (isCute)
+//			result++;
+//		else
+//			result--;
+//	}
+//	if (0 < result)
+//		cout << "Junhee is cute!";
+//	else
+//		cout << "Junhee is not cute!";
+//}
