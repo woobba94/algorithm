8a15d3815bd2bc60db2cf78bc5796b0209eb1142
woobba94 <jwj3199@naver.com>
2021-08-22 23:49:45 +0900

    0302 clear

---
+++ Project_codingTest/0320_1952.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int y, x;
+//	cin >> y >> x;
+//
+//	if (x < y)
+//	{
+//		cout << ((x - 1) * 2) + 1;
+//	}
+//	else if(x > y)
+//	{
+//		cout << (y - 1) * 2;
+//	}
+//	else
+//	{
+//		if(x == 2)
+//			cout << 2;
+//		else
+//			cout << (x - 1) * 2;
+//	}
+//}
