f1634ea86ad8a11d38547f857f2ed8990da022c5
woobba94 <jwj3199@naver.com>
2021-08-23 23:32:31 +0900

    0322 clear

--- Project_codingTest/0322_14719.cpp
+++ Project_codingTest/0322_14719.cpp
@@ -1,87 +1,54 @@
 //#include <iostream>
-//
-//#include <algorithm>
-//
 //using namespace std;
 //
-//
-//
-//const int MAX = 1000000000 + 1;
-//
-//
-//
-//int N;
-//
-//int number[12], Operator[4];
-//
-//int maxResult = -MAX, minResult = MAX;
-//
-//
-//
-//void DFS(int plus, int minus, int multiply, int divide, int cnt, int sum)
-//
+//int main()
 //{
+//	int y, x;
+//	cin >> y >> x;
 //
-//    //연산자를 모두 사용하였을 경우
+//	int* arr = new int[x];
+//	for (int i = 0; i < x; i++)
+//	{
+//		cin >> arr[i];
+//	}
 //
-//    if (cnt == N)
+//	bool isUp;
 //
-//    {
+//	int max = arr[0];
+//		
+//	int count = 0;
+//	for (int i = 1; i < x - 1; i++)
+//	{
+//		if (i == 1)
+//		{
+//			if (arr[0] <= arr[1])
+//				isUp = true;
+//			else
+//				isUp = false;
+//		}
 //
-//        maxResult = max(maxResult, sum);
+//		if (arr[i] < arr[i + 1])
+//		{
+//			// 아래 꼭지점
+//			if (isUp == false && arr[i] < max)
+//			{
+//				isUp = true;
+//				count++;
+//				arr[i]++;
+//				i = 0;
+//			}
+//		}
+//		else
+//		{
+//			// 위 꼭지점 -> max 갱신
+//			if (isUp == true && arr[i] > max)
+//			{
+//				max = arr[i];
+//			}
+//			isUp = false;
+//		}
+//	}
 //
-//        minResult = min(minResult, sum);
-//
-//    }
-//
-//
-//
-//    //모든 경우를 고려
-//
-//    if (plus > 0)
-//
-//        DFS(plus - 1, minus, multiply, divide, cnt + 1, sum + number[cnt]);
-//
-//    if (minus > 0)
-//
-//        DFS(plus, minus - 1, multiply, divide, cnt + 1, sum - number[cnt]);
-//
-//    if (multiply > 0)
-//
-//        DFS(plus, minus, multiply - 1, divide, cnt + 1, sum * number[cnt]);
-//
-//    if (divide > 0)
-//
-//        DFS(plus, minus, multiply, divide - 1, cnt + 1, sum / number[cnt]);
-//
-//}
-//
-//
-//
-//int main(void)
-//
-//{
-//
-//    cin >> N;
-//
-//    for (int i = 0; i < N; i++)
-//
-//        cin >> number[i];
-//
-//    for (int i = 0; i < 4; i++)
-//
-//        cin >> Operator[i];
-//
-//
-//
-//    DFS(Operator[0], Operator[1], Operator[2], Operator[3], 1, number[0]);
-//
-//
-//
-//    cout << maxResult << endl;
-//
-//    cout << minResult << endl;
-//
-//    return 0;
+//	cout << count;
 //
 //}
