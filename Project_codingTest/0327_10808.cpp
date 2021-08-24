8a0e5d97b663fbea0d5f8c8fbbda8b8a00a5d79d
woobba94 <jwj3199@naver.com>
2021-08-25 01:23:45 +0900

    0327 clear

---
+++ Project_codingTest/0327_10808.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	string input;
+//	cin >> input;
+//
+//	int count[26] = {};
+//
+//	for (int i = 0; i < input.length(); i++)
+//	{
+//		count[input[i] - 'a']++;
+//	}
+//
+//	for (int i = 0; i < 26; i++)
+//	{
+//		cout << count[i] << " ";
+//	}
+//}
