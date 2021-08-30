88b9eb79f6fe9963d6d885dbb13f1bcfb107076f
woobba94 <jwj3199@naver.com>
2021-08-31 01:22:31 +0900

    0347 clear

---
+++ Project_codingTest/0347_6131.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int count = 0;
+//	for (int i = 1; i < 501; i++)
+//	{
+//		for (int j = 1; j < 501; j++)
+//		{
+//			if (i * i == j * j - n) count++;
+//		}
+//	}
+//	cout << count;
+//}
