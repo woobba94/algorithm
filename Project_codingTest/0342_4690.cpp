b78059842da7d3be55030a45271c22f34a6bf0d6
woobba94 <jwj3199@naver.com>
2021-08-31 00:47:54 +0900

    0342 clear

---
+++ Project_codingTest/0342_4690.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	for (int i = 1; i < 101; i++)
+//	{
+//		for (int j = 2; j < 101; j++)
+//		{
+//			for (int k = j + 1; k < 101; k++)
+//			{
+//				for (int l = k + 1; l < 101; l++)
+//				{
+//					if (i * i * i == j * j * j + k * k * k + l * l * l)
+//					{
+//						cout << "Cube = " << i << ", Triple = (" << j 
+//							 << "," << k << "," << l << ")" << endl;
+//					}
+//				}
+//			}
+//		}
+//	}
+//}
