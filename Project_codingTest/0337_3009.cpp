0b465f7b39c3f6bec0cef47605aa133929815c04
woobba94 <jwj3199@naver.com>
2021-08-30 19:11:53 +0900

    0337 clear

---
+++ Project_codingTest/0337_3009.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int x1, y1, x2, y2, x3, y3;
+//	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+//
+//	if (x1 != x2)
+//	{
+//		if (x1 != x3) cout << x1;
+//		else cout << x2;
+//	}
+//	else cout << x3;
+//	cout << " ";
+//	if (y1 != y2)
+//	{
+//		if (y1 != y3) cout << y1;
+//		else cout << y2;
+//	}
+//	else cout << y3;
+//
+//}
