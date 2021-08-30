23a5c30f54467b5c4dea3707a071783547567bb0
woobba94 <jwj3199@naver.com>
2021-08-30 19:07:10 +0900

    0336 clear

---
+++ Project_codingTest/0336_3053.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	double R;
+//	cin >> R;
+//
+//	cout << fixed;
+//	cout.precision(6);
+//	// 유클리드 기하학
+//	cout << R * R * 3.141592653589 << endl;
+//
+//	// 택시 기하학
+//	cout << R * R / 2 * 4;
+//}
