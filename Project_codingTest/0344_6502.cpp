ab7cdab2fda8bdcd834ce3ed9c23ab333b09dbb3
woobba94 <jwj3199@naver.com>
2021-08-31 01:07:49 +0900

    0344 clear

---
+++ Project_codingTest/0344_6502.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int count = 1;
+//	while (1)
+//	{
+//		int r, w, l;
+//		cin >> r;
+//		if (r == 0) break;
+//		cin >> w >> l;
+//		cout << "Pizza " << count;
+//		if (w * w + l * l <= (2 * r) * (2 * r)) cout << " fits on the table.";
+//		else cout << " does not fit on the table.";
+//		cout << endl;
+//		count++;
+//	}
+//}
