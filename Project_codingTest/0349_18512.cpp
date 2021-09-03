fa4fa1262d528043bac8a565a316527040dd08ee
woobba94 <jwj3199@naver.com>
2021-09-03 19:54:30 +0900

    0349 clkear

---
+++ Project_codingTest/0349_18512.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a, aStart;
+//	int b, bStart;
+//
+//	cin >> a >> b >> aStart >> bStart;
+//
+//	
+//	bool flag = false;
+//
+//	for (int i = 0 ; i < 100; i++)
+//	{
+//		if (aStart == bStart)
+//		{
+//			flag = true;
+//			break;
+//		}
+//
+//		if (aStart + a > bStart + b)
+//			bStart += b;
+//		else
+//			aStart += a;
+//	}
+//
+//	if (flag)
+//		cout << aStart;
+//	else
+//		cout << -1;
+//
+//}
