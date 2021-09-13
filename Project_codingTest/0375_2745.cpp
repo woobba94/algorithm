56f646b754e9c9231aedc2760c21b30aa5711f13
woobba94 <jwj3199@naver.com>
2021-09-13 20:11:27 +0900

    2745 re clear

---
+++ Project_codingTest/0375_2745.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int base;
+//	string input;
+//	cin >> input >> base;
+//	int answer = 0;
+//	for (int i = 0; i < input.length(); i++)
+//	{
+//		if(input[i] >= 65)
+//			answer = answer * base + input[i] - 'A' + 10;
+//		else
+//			answer = answer * base + input[i] - '0';
+//	}
+//		
+//
+//	cout << answer;
+//}
