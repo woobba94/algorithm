2dfa7049c7fe36b022c2f558ac16e0989adafc98
woobba94 <jwj3199@naver.com>
2021-09-18 22:17:40 +0900

    0386 clear

---
+++ Project_codingTest/0386_2744.cpp
@@ -0,0 +1,23 @@
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string input;
+//	cin >> input;
+//
+//	int size = input.length();
+//
+//
+//	for (int i = 0; i < size; i++)
+//	{
+//		// 소문자면
+//		if (input[i] >= 97)
+//			input[i] -= 'a' - 'A';
+//		else
+//			input[i] += 'a' - 'A';
+//	}
+//
+//	cout << input;
+//}
