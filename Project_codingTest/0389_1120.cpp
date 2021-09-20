8586313af0d6f78a9e6fc77e81bcc0832c09538b
woobba94 <66201264+woobba94@users.noreply.github.com>
2021-09-20 19:15:19 +0900

    0389 clear

--- Project_codingTest/0389_1120.cpp
+++ Project_codingTest/0389_1120.cpp
@@ -7,9 +7,31 @@
 //
 //	cin >> a >> b;
 //
-//	int size = b.length();
-//	for (int i = 0; i < size; i++)
+//	int aSize = a.length();
+//	int bSize = b.length();
+//	
+//	int minDistance = aSize; // 모두 다르다고 가정
+//	
+//	for(int i = 0 ; i < bSize; i++)
 //	{
+//		int index = i;
+//		if (i + aSize > bSize) break;
+//		int distance = 0;
+//		for (int j = 0; j < aSize; j++)
+//		{
+//			if (bSize <= index) break;
+//			if (a[j] != b[index])
+//			{
+//				distance++;
+//			}
+//			index++;
+//		}
 //
+//		if (minDistance > distance)
+//		{
+//			minDistance = distance;
+//		}
 //	}
+//	cout << minDistance;
+//	
 //}
