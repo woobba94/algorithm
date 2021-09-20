64cc7d2446fdef9e6ddc59b11f3ec9efd117b180
woobba94 <jwj3199@naver.com>
2021-09-20 14:33:21 +0900

    0389 hold

---
+++ Project_codingTest/0389_1120.cpp
@@ -0,0 +1,15 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	string a, b;
+//
+//	cin >> a >> b;
+//
+//	int size = b.length();
+//	for (int i = 0; i < size; i++)
+//	{
+//
+//	}
+//}
