0274a5b103bad37707d723f5f7d058175e68729b
woobba94 <jwj3199@naver.com>
2021-09-20 14:15:13 +0900

    0388 clear

---
+++ Project_codingTest/0388_5598.cpp
@@ -0,0 +1,18 @@
+//#include<bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	string input;
+//	cin >> input;
+//	int size = input.length();
+//	for (int i = 0; i < size; i++)
+//	{
+//		input[i] -= 3;
+//		if (input[i] < 'A')
+//		{
+//			input[i] += ('Z' - 'A') + 1;
+//		}
+//	}
+//	cout << input;
+//}
