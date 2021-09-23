90e5eefdc3887d515f48cd8d3fd1e9c8904eb1e4
woobba94 <jwj3199@naver.com>
2021-09-23 21:56:44 +0900

    0392 clear

---
+++ Project_codingTest/0392_10798.cpp
@@ -0,0 +1,24 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//const int MAX_SIZE = 5;
+//
+//int main()
+//{
+//	string arr[MAX_SIZE];
+//
+//	for (int i = 0; i < MAX_SIZE; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//
+//	for (int j = 0; j < 15; j++)
+//	{
+//		for (int i = 0; i < MAX_SIZE; i++)
+//		{
+//			if (arr[i].length() <= j) continue;
+//			cout << arr[i][j];
+//		}
+//	}
+//	
+//}
