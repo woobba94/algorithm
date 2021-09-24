fa792a06286a042d0d40fdb6ee5e0dc6d3e02bc6
woobba94 <jwj3199@naver.com>
2021-09-24 17:37:38 +0900

    0401 clear

---
+++ Project_codingTest/0401_10987.cpp
@@ -0,0 +1,20 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	string s;
+//	cin >> s;
+//	int len = s.length();
+//	int count = 0;
+//	for (int i = 0; i < len; i++)
+//	{
+//		if (s[i] == 'a' ||
+//			s[i] == 'e' ||
+//			s[i] == 'i' ||
+//			s[i] == 'o' ||
+//			s[i] == 'u')
+//			count++;
+//	}
+//	cout << count;
+//}
