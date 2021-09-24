489f412536d78e2da81fc476f592db1f419549c6
woobba94 <jwj3199@naver.com>
2021-09-24 17:30:26 +0900

    0440 clear

---
+++ Project_codingTest/0400_4458.cpp
@@ -0,0 +1,21 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	cin.ignore();
+//	string* s = new string[n];
+//	for (int i = 0; i < n; i++)
+//	{
+//		getline(cin, s[i]);
+//	}
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		if (s[i][0] >= 'a')
+//			s[i][0] -= 'a' - 'A';
+//		cout << s[i] << endl;
+//	}
+//}
