98718285b9edcbdb174f283c5d04514605b88eea
woobba94 <jwj3199@naver.com>
2021-09-24 17:49:33 +0900

    0402 clear

---
+++ Project_codingTest/0402_2754.cpp
@@ -0,0 +1,27 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	char input[3];
+//	cin >> input;
+//	int i, j;
+//	if (input[0] == 'A') i = 4;
+//	else if (input[0] == 'B') i = 3;
+//	else if (input[0] == 'C') i = 2;
+//	else if (input[0] == 'D') i = 1;
+//	else
+//	{
+//		cout << "0.0";
+//		return 0;
+//	}
+//
+//	if (input[1] == '+') j = 3;
+//	else if (input[1] == '0') j = 0;
+//	else
+//	{
+//		i--; 
+//		j = 7;
+//	}
+//	cout << i << '.' << j;
+//}
