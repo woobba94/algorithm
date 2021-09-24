0ff62b941ef147830e3435c598b39280d09b1c78
woobba94 <jwj3199@naver.com>
2021-09-24 19:18:03 +0900

    0407 clear

---
+++ Project_codingTest/0407_5218.cpp
@@ -0,0 +1,27 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int t;
+//	cin >> t;
+//
+//	while (t--)
+//	{
+//		cout << "Distances: ";
+//		string first, second;
+//		cin >> first >> second;
+//
+//		int len = first.length();
+//		for (int i = 0; i < len; i++)
+//		{
+//			if (second[i] >= first[i])
+//				cout << second[i] - first[i];
+//			else
+//				cout << (second[i] + 26) - first[i];
+//
+//			cout << ' ';
+//		}
+//		cout << endl;
+//	}
+//}
