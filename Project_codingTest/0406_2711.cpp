03f64ccc105f88d3841dfed28b867f40115d0df2
woobba94 <jwj3199@naver.com>
2021-09-24 19:08:49 +0900

    0406 clear

---
+++ Project_codingTest/0406_2711.cpp
@@ -0,0 +1,22 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	while (n--)
+//	{
+//		int target;
+//		string s;
+//		cin >> target >> s;
+//
+//		int len = s.length();
+//		for (int i = 0; i < len; i++)
+//		{
+//			if (i == target - 1) continue;
+//			cout << s[i];
+//		}
+//		cout << endl;
+//	}
+//}
