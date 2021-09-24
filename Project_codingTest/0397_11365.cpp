2f86d9da86ab5048d5e576fc881d4a533fd9a68c
woobba94 <jwj3199@naver.com>
2021-09-24 16:38:57 +0900

    0397 clear

---
+++ Project_codingTest/0397_11365.cpp
@@ -0,0 +1,21 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//const int MAX_SIZE = 501;
+//int main()
+//{
+//	while (1)
+//	{
+//		string input;
+//		getline(cin, input);
+//
+//		if (input == "END") break;
+//
+//		int len = input.length();
+//		for (int i = len - 1; i >= 0; i--)
+//		{
+//			cout << input[i];
+//		}
+//
+//		cout << endl;
+//	}
+//}
