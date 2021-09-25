0359054d352c33dc3715c7f9a33fc6d9fecfd25d
woobba94 <jwj3199@naver.com>
2021-09-25 16:21:23 +0900

    0408 clear

---
+++ Project_codingTest/0408_9086.cpp
@@ -0,0 +1,15 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int t;
+//	cin >> t;
+//	while (t--)
+//	{
+//		string input;
+//		cin >> input;
+//		int endIndex = input.length() - 1;
+//		cout << input[0] << input[endIndex] << endl;
+//	}
+//}
