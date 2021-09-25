acfe18a5027f0503f78b1a6be178b9e39c502f9d
woobba94 <jwj3199@naver.com>
2021-09-25 17:01:29 +0900

    0412 clear

---
+++ Project_codingTest/0412_10821.cpp
@@ -0,0 +1,15 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	string input;
+//	cin >> input;
+//	int len = input.length();
+//	int count = 1;
+//	for (int i = 0; i < len; i++)
+//	{
+//		if (input[i] == ',') count++;
+//	}
+//	cout << count;
+//}
