0e49270f266e7a31fdfd694d4784a70151b5f22f
woobba94 <jwj3199@naver.com>
2021-09-25 21:51:49 +0900

    0420 clear

---
+++ Project_codingTest/0420_6321.cpp
@@ -0,0 +1,23 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int n, temp = 0;
+//	cin >> n;
+//	while (temp < n)
+//	{
+//		string s;
+//		cin >> s;
+//		int len = s.length();
+//		cout << "String #" << temp + 1 << endl;
+//		for (int i = 0; i < len; i++)
+//		{
+//			if (s[i] == 'Z') cout << 'A';
+//			else cout << (char)(s[i] + 1);
+//		}
+//		cout << endl;
+//		cout << endl;
+//		temp++;
+//	}
+//}
