c9836457198c1817fedb73bfe407081143068e6b
woobba94 <jwj3199@naver.com>
2021-09-25 16:38:26 +0900

    0410 clear

---
+++ Project_codingTest/0410_10102.cpp
@@ -0,0 +1,22 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int score = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		char input;
+//		cin >> input;
+//		if (input == 'A') score--;
+//		else score++;
+//	}
+//	if (score)
+//	{
+//		if (score < 0) cout << 'A';
+//		else cout << 'B';
+//	}
+//	else cout << "Tie";
+//}
