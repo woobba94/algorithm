e93f736725a886ec05f8c265e69e0892060ff93a
woobba94 <jwj3199@naver.com>
2021-09-25 21:10:53 +0900

    0417 clear

---
+++ Project_codingTest/0417_11098.cpp
@@ -0,0 +1,26 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	while(n--)
+//	{
+//		int p;
+//		cin >> p;
+//		int maxCost = 0;
+//		string target;
+//		for (int i = 0; i < p; i++)
+//		{
+//			int cost;
+//			string name;
+//			cin >> cost >> name;
+//			if (cost > maxCost)
+//			{
+//				maxCost = cost;
+//				target = name;
+//			}
+//		}
+//		cout << target << endl;
+//	}
+//}
