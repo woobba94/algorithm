c88d4ad6c0a6d6d00e0f4ab00173543a0d56c11e
woobba94 <jwj3199@naver.com>
2021-09-25 22:06:15 +0900

    0422 clear

---
+++ Project_codingTest/0422_13163.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	cin.ignore();
+//	while (n--)
+//	{
+//		string name;
+//		getline(cin, name);
+//		int len = name.length();
+//		bool flag = false;
+//		cout << "god";
+//		for (int i = 0; i < len; i++)
+//		{
+//			if (name[i] == ' ')	
+//				flag = true;
+//			if (flag == true && name[i] != ' ')
+//				cout << name[i];
+//		}
+//		cout << endl;
+//	}
+//}
