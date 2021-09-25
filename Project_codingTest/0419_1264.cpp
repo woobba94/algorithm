7a20f18d77852f41ca2d69c3269b9ec4b95ffc49
woobba94 <jwj3199@naver.com>
2021-09-25 21:37:28 +0900

    0419 clear

---
+++ Project_codingTest/0419_1264.cpp
@@ -0,0 +1,29 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	while (1)
+//	{
+//		string s;
+//		getline(cin, s);
+//		if (s[0] == '#')break;
+//		int count = 0;
+//		int len = s.length();
+//		for (int i = 0; i < len; i++)
+//		{
+//			if (s[i] == 'A' ||
+//				s[i] == 'E' ||
+//				s[i] == 'I' ||
+//				s[i] == 'O' ||
+//				s[i] == 'U' ||
+//				s[i] == 'a' ||
+//				s[i] == 'e' ||
+//				s[i] == 'i' ||
+//				s[i] == 'o' ||
+//				s[i] == 'u')
+//				count++;
+//		}
+//		cout << count << endl;
+//	}
+//}
