27bbd233d4ac422868e1eb4db0fb681bdfb13684
woobba94 <jwj3199@naver.com>
2021-09-26 18:38:48 +0900

    0429 clear

---
+++ Project_codingTest/0426_1013.cpp
@@ -0,0 +1,17 @@
+//#include <bits/stdc++.h>
+//#include <regex>
+//using namespace std;
+//
+//int main()
+//{
+//	int t;
+//	cin >> t;
+//	while (t--)
+//	{
+//		string s;
+//		cin >> s;
+//		regex pattern("(100+1+|01)+");
+//		if (regex_match(s, pattern))cout << "YES\n";
+//		else cout << "NO\n";
+//	}
+//}
