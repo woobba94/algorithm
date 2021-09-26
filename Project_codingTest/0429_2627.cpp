837628ca9994ce92eeb6d47d7e324171be455cfd
woobba94 <jwj3199@naver.com>
2021-09-26 19:24:48 +0900

    0429 clear

---
+++ Project_codingTest/0429_2627.cpp
@@ -0,0 +1,11 @@
+//#include <bits/stdc++.h>
+//#include <regex>
+//using namespace std;
+//
+//int main()
+//{
+//	regex pattern("(100+1+|01)+");
+//	string input;
+//	cin >> input;
+//	cout << (regex_match(input, pattern) ? "SUBMARINE" : "NOISE");
+//}
