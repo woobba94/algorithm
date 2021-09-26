0827caf75db51a693fee610639bce30c76e7a2ee
woobba94 <jwj3199@naver.com>
2021-09-26 19:31:48 +0900

    0430 clear

---
+++ Project_codingTest/0430_9342.cpp
@@ -0,0 +1,17 @@
+//#include <bits/stdc++.h>
+//#include <regex>
+//using namespace std;
+//
+//int main()
+//{
+//	regex pattern("(A|B|C|D|E|F)?A+F+C+(A|B|C|D|E|F)?");
+//	int t;
+//	cin >> t;
+//	while (t--)
+//	{
+//		string input;
+//		cin >> input;
+//		cout << 
+//			(regex_match(input, pattern) ? "Infected!\n" : "Good\n");
+//	}
+//}
