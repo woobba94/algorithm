9d0e3ab28e0e6477833fe90dbfa4fae4c58b0972
woobba94 <jwj3199@naver.com>
2021-10-01 19:58:54 +0900

    0437 clear

---
+++ Project_codingTest/0437_11656.cpp
@@ -0,0 +1,23 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	map<string, bool> arr;
+//
+//	string s;
+//	cin >> s;
+//	int len = s.length();
+//	
+//	for (int i = 0; i < len; i++)
+//	{
+//		string temp = s.substr(i, len);
+//		arr[temp] = true;
+//	}
+//
+//	for (auto iter = arr.begin(); iter != arr.end();)
+//	{
+//		cout << iter->first << '\n';
+//		iter++;
+//	}
+//}
