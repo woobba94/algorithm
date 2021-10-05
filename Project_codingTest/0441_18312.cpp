5b7bcdaa003457b690d3c365742e6c97c46f75b0
woobba94 <jwj3199@naver.com>
2021-10-05 22:56:17 +0900

    0441 clear

--- Project_codingTest/0441_18312.cpp
+++ Project_codingTest/0441_18312.cpp
@@ -1,37 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-	int N, K;
-	cin >> N >> K;
-	
-	int count = 0;
-
-	for (int i = 0; i < N; i++)
-	{
-		string time;
-
-		if (i < 10) time += "0";
-		time += i + '0';
-
-		for (int j = 0; j < 60; j++)
-		{
-			if (j < 10) time += "0";
-			time += j + '0';
-
-			for (int k = 0; k < 60; k++)
-			{
-				if (k < 10) time += "0";
-				time += k + '0';
-			}
-		}
-		
-		for (int j = 0; j < 6; j++)
-		{
-			if (time[i] == K + '0') count++;
-		}
-	}
-
-	cout << count;
-}
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int N, K;
+//	cin >> N >> K;
+//	int searchSize = (N + 1) * 60 * 60;
+//	string time;
+//	int count = 0;
+//
+//	for (int i = 0; i < searchSize; i++)
+//	{
+//		time = "";
+//		int temp = i;
+//		if (temp / 3600 < 10) time += "0";
+//		time += to_string(temp / 3600);
+//		temp %= 3600;
+//
+//		if (temp / 60 < 10) time += "0";
+//		time += to_string(temp / 60);
+//		temp %= 60;
+//
+//		if(temp < 10) time += "0";
+//		time += to_string(temp);
+//
+//		for (int j = 0; j < 6; j++)
+//		{
+//			if (time[j] == K + '0')
+//			{
+//				count++;
+//				break;
+//			}
+//		}
+//	}
+//
+//	cout << count;
+//}
