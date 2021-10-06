daa7e9b357248bd22f7bf75c32b85cb73234d59d
woobba94 <jwj3199@naver.com>
2021-10-06 21:43:54 +0900

    0442 clear

---
+++ Project_codingTest/0442_1813.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int arr[100001] = {};
+//
+//int main()
+//{
+//	int n, k;
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> k;
+//		arr[k]++;
+//	}
+//
+//	for (int i = n; i >= 0; i--)
+//	{
+//		if (arr[i] == i)
+//		{
+//			cout << i;
+//			return 0;
+//		}
+//	}
+//	cout << -1;
+//}
