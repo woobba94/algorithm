b675a70d1bb38d9e6958796aae1089e3820aa9b9
woobba94 <jwj3199@naver.com>
2021-10-06 22:19:57 +0900

    0444 clear

---
+++ Project_codingTest/0444_15881.cpp
@@ -0,0 +1,30 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	string s;
+//	cin >> s;
+//	int count = 0;
+//
+//	for (int i = 0; i < n - 3; i++)
+//	{
+//		if (s[i] == 'p')
+//		{
+//			if (s[i + 1] == 'P')
+//			{
+//				if (s[i + 2] == 'A')
+//				{
+//					if (s[i + 3] == 'p')
+//					{
+//						count++;
+//						i += 3;
+//					}
+//				}
+//			}
+//		}
+//	}
+//	cout << count;
+//}
