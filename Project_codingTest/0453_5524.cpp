35846b2713690bfc969533b71668d3d2ed368bec
woobba94 <jwj3199@naver.com>
2021-10-11 22:51:48 +0900

    0453 clear

---
+++ Project_codingTest/0453_5524.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//
+//	while (n--)
+//	{
+//		string name;
+//		cin >> name;
+//
+//		for (int i = 0; i < name.size(); i++)
+//		{
+//			// 대문자면
+//			if (name[i] < 97)
+//			{
+//				name[i] += 'a' - 'A';
+//			}
+//		}
+//
+//		cout << name << endl;
+//	}
+//}
